eb125f5b86c2324fb02b2ff2e9a65416b8d239ea
garrett krage <garrett.krage@gmail.com>
2019-12-22 21:58:19 -0700

    added data-hour in script.js tweaked the html.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var moment = moment().format('YYYY MM DD');
-console.log(typeof(moment))
+var hour = ['9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
+// adds the attribute data hour = number for a unique identifier
+for (i = 0; i < hour.length; i++){
+    var index = $('.input-group-prepend')[i]
+    $(index).attr('data-hour', [i]);
+}
