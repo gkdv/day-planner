257ec70cb099805e6d5ccfd9633459d5472fe80d
garrett krage <garrett.krage@gmail.com>
2019-12-27 15:05:21 -0700

    added text box in content div.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var hour = ['9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 
 // adds the attribute data hour = number for a unique identifier
 for (i = 0; i < hour.length; i++){
-    var index = $('.individual-time')[i]
+    var index = $('.content-box')[i]
     $(index).attr('data-hour', [i]);
 }
 
@@ -17,8 +17,7 @@ for (i = 0; i < hour.length; i++){
 for (i = 0; i < hour.length; i++){
     var index = $('.save-content')[i];
     $(index).prepend(`<img id="save-image" src="assets/icons8-save-25.png" ></img>`)
-    
-    // index.style.width = '50px';
-    // index.style.height = '50px';
 
 }
+
+// adds on click event for text input for each hour.
