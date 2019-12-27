95e9916f91b56dac1708358977a473773cfaa92f
garrett krage <garrett.krage@gmail.com>
2019-12-27 16:57:08 -0700

    local storage key value pairs for input set up

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var moment = moment().format('YYYY MM DD');
 var hour = ['9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 
-// adds the attribute data hour = number for a unique identifier
+// adds edit.png to save content column.
 for (i = 0; i < hour.length; i++){
-    var index = $('.content-box')[i]
-    $(index).attr('data-hour', [i]);
+    var index = $('.save-content')[i];
+    $(index).prepend(`<img id="save-image" src="assets/icons8-save-25.png" ></img>`)
+    
 }
 
 // update the text field of the Hour of the day div.
@@ -13,11 +14,22 @@ for (i = 0; i < hour.length; i++){
     $(index).text(hour[i])
 }
 
-// adds edit.png to save content column.
+// adds the attribute data hour = i to form control class.
 for (i = 0; i < hour.length; i++){
-    var index = $('.save-content')[i];
-    $(index).prepend(`<img id="save-image" src="assets/icons8-save-25.png" ></img>`)
-
+    var indexText = $('.form-control')[i]
+    $(indexText).attr('data-hour', [i]);
+    
+}
+// adds the attribute data hour = i to img element.
+for (i = 0; i < hour.length; i++){
+    var indexSave = $('img')[i]
+    $(indexSave).attr('data-hour', [i]);
 }
 
-// adds on click event for text input for each hour.
+
+// on click event for saving text to local memory.
+$('img').on('click', function(){
+    var key = $(this).attr('data-hour');
+    var value = $(this).parent().siblings('.content-box').children().val();
+    localStorage.setItem(key, value)
+})
