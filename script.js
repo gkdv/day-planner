8ea0243145b43c59b18f897f9d614521f8226f1a
garrett krage <garrett.krage@gmail.com>
2019-12-27 12:35:30 -0700

    added data-hour attr. for each row. updated the text for the time col. respectively.

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var hour = ['9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 
 // adds the attribute data hour = number for a unique identifier
 for (i = 0; i < hour.length; i++){
-    var index = $('.input-group-prepend')[i]
+    var index = $('.individual-time')[i]
     $(index).attr('data-hour', [i]);
 }
 
 // update the text field of the Hour of the day div.
 for (i = 0; i < hour.length; i++){
-    var index = $('.input-group-prepend')[i]
+    var index = $('.time-col')[i]
     $(index).text(hour[i])
 }
