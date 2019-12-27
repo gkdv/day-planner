91fe27a9007fbef1b75e4d0fceaf397cefb61ee5
garrett krage <garrett.krage@gmail.com>
2019-12-27 13:55:31 -0700

    center align footer text and cleaned up save image UI.

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ for (i = 0; i < hour.length; i++){
     var index = $('.time-col')[i]
     $(index).text(hour[i])
 }
+
+// adds edit.png to save content column.
+for (i = 0; i < hour.length; i++){
+    var index = $('.save-content')[i];
+    $(index).prepend(`<img id="save-image" src="assets/icons8-save-25.png" ></img>`)
+    
+    // index.style.width = '50px';
+    // index.style.height = '50px';
+
+}
