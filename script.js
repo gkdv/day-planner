0b49755c9a9e39283cf84acf20db87bc552923f5
garrett krage <garrett.krage@gmail.com>
2019-12-29 11:40:32 -0700

    local storage writes to respective content box. local storage code is complete. update content box color based on time is complete.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var moment = moment().format('YYYY MM DD');
-var hour = ['9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+var moment = moment().format("MM ddd, YYYY HH:mm:ss a");
+var hourText = ['9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+var hour = ['9','10', '11', '12', '13', '14', '15', '16', '17'];
+var currentHour = moment.substring(13,15);
+
+
+//grabs any information within local storage.
+for (i = 0; i < hour.length; i++){
+    var value = localStorage.getItem([i]);
+    var contentText = ($('input')[i]);
+    $(contentText).val(value);
+}
 
 // adds edit.png to save content column.
 for (i = 0; i < hour.length; i++){
@@ -11,13 +21,13 @@ for (i = 0; i < hour.length; i++){
 // update the text field of the Hour of the day div.
 for (i = 0; i < hour.length; i++){
     var index = $('.time-col')[i]
-    $(index).text(hour[i])
+    $(index).text(hourText[i])
 }
 
-// adds the attribute data hour = i to form control class.
+// adds the attribute data hour = hour[i] to form control class.
 for (i = 0; i < hour.length; i++){
     var indexText = $('.form-control')[i]
-    $(indexText).attr('data-hour', [i]);
+    $(indexText).attr('data-hour', hour[i]);
     
 }
 // adds the attribute data hour = i to img element.
@@ -33,3 +43,19 @@ $('img').on('click', function(){
     var value = $(this).parent().siblings('.content-box').children().val();
     localStorage.setItem(key, value)
 })
+
+//set background color of content box based on current hour.
+function contentColor (){
+    for (i = 0; i < hour.length; i++){
+        var formControl = $('.form-control')[i];
+        var dataHour = parseInt($(formControl).attr('data-hour'));
+        var formControlColor = $('.form-control')[i];
+        if (dataHour <= currentHour){
+            $(formControlColor).css('background-color', '#e64545');
+        }
+    }
+}
+
+// calls contentColor function every second.
+setInterval(contentColor, 1000);
+
